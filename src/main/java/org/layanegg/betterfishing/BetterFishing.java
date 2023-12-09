5a0a5de5cb10e3d32c3d5e2f277f425a19e8fbab
mcfiegel <mcfiegel@mail.com>
2023-12-09 04:59:13 -0500

    Added a silly little feature

--- src/main/java/org/layanegg/betterfishing/BetterFishing.java
+++ src/main/java/org/layanegg/betterfishing/BetterFishing.java
@@ -1,17 +1,27 @@
 package org.layanegg.betterfishing;
 
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.layanegg.betterfishing.listeners.XPBottleBreakListener;
 
-public final class BetterFishing extends JavaPlugin {
+public final class BetterFishing extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        getServer().getPluginManager().registerEvents(this, this);
+        getServer().getPluginManager().registerEvents(new XPBottleBreakListener(), this);
     }
 
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent e){
+        e.setJoinMessage("What's up! did this work?");
+    }
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+
     }
 }
--- src/main/java/org/layanegg/betterfishing/listeners/XPBottleBreakListener.java
+++ src/main/java/org/layanegg/betterfishing/listeners/XPBottleBreakListener.java
@@ -1,2 +1,24 @@
-package org.layanegg.betterfishing.listeners;public class XPBottleBreakListener {
+package org.layanegg.betterfishing.listeners;
+
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.ExpBottleEvent;
+
+public class XPBottleBreakListener implements Listener {
+
+    @EventHandler
+    public void onXPBottleBreak(ExpBottleEvent e){
+        Block block = e.getHitBlock();
+        BlockFace blockFace = e.getHitBlockFace();
+        assert block != null;
+        assert blockFace != null;
+        try {
+            block.applyBoneMeal(blockFace);
+
+        }catch (NullPointerException exception){
+            System.out.println("NULL POINTER EXCEPTION");
+        }
+    }
 }
